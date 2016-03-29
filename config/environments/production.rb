85c159289dd1759d9d8192bc457016592a917351
Marlon <marlonvpaula@gmail.com>
2016-03-29 13:16:14 -0300

    config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -28,7 +28,7 @@ Folhaprocmultipet::Application.configure do
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect'
-  config.assets.compile = true
+  config.assets.compile = false
   
   # Generate digests for assets URLs.
   config.assets.digest = true
